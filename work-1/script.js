8f8f50c446fedddcb12664e268c83a4a70c6c25c
sem6703 <sem6703@yandex.ru>
2021-06-14 15:46:54 +0300

    home work 3.1

---
+++ work-1/script.js
@@ -0,0 +1,12 @@
+const name = prompt('Введите имя: ');
+const sName = prompt('Введите фамилию: ');
+const age = +prompt('Введите возраст: ');
+
+const user = {
+	name,
+	sName,
+	age
+};
+
+console.log('OK');
+alert(user);
